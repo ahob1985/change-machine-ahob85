b73206f8464759adbfa191df7f3640234cdb5dc1
Aaron Hobson <aaron@ahob85.com>
2021-08-17 11:02:36 -0700

    challenge complete

--- ChangeMachine.java
+++ ChangeMachine.java
@@ -11,7 +11,21 @@ public class ChangeMachine
 
     public static void main(String[] args) 
     {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Change Machine!");
+        System.out.print("Please enter cents as a positive integer: ");
+        int cents = sc.nextInt();
+        System.out.println(cents + " cents makes:");
+        int quarters = cents / 25;
+        cents = cents - quarters * 25;
+        int dimes = cents / 10;
+        cents = cents - dimes * 10;
+        int nickels = cents / 5;
+        cents = cents - nickels * 5;
+        System.out.println(quarters + " quarters");
+        System.out.println(dimes + " dimes");
+        System.out.println(nickels + " nickels");
+        System.out.println(cents + " pennies");
     }
 
 }
